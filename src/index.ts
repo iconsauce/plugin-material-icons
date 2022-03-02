d39b341a19eef2aedc65d0e199fe36db81aa0722
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-02 18:07:51 +0100

    feat: add material-icons plugin

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { dirname } from 'path'
 import { IconsaucePlugin } from '@iconsauce/plugin'
 
-const mdiSvgPlugin: IconsaucePlugin = {
-  prefix: 'mdi',
+const materialIconsPlugin: IconsaucePlugin = {
+  prefix: 'mi',
   regex: {
-    code: /(mdi)(\/{1}[0-9a-z-]+){1,2}/gm,
-    lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\.svg)/,
+    code: /(mi)(\/[0-9a-z-]+){2}/gm,
+    lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\/(baseline|outline|round|sharp)\.svg)/,
   },
-  selector: (path: RegExpMatchArray) => `mdi/${path[2].replace(/[_]+/g, '-')}`,
-  path: `${dirname(require.resolve('@mdi/svg/package.json'))}/**/*.svg`,
+  selector: (path: RegExpMatchArray) => `mi/${path[3]}/${path[2].replace(/[_]+/g, '-')}`,
+  path: `${dirname(require.resolve('@material-icons/svg/package.json'))}/svg/**/@(baseline|outline|round|sharp).svg`,
 }
 
-export default mdiSvgPlugin
-module.exports = mdiSvgPlugin
+export default materialIconsPlugin
+module.exports = materialIconsPlugin
