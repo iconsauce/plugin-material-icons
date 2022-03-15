8734e15cadad4f15fd2a554f4f892d4f8e25f88b
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-15 10:46:16 +0100

    fix: ease how modules are resoved

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { join, resolve } from 'path'
+import { resolve } from 'path'
 import { IconsaucePlugin } from '@iconsauce/plugin'
 
 const materialIconsPlugin: IconsaucePlugin = {
@@ -8,7 +8,7 @@ const materialIconsPlugin: IconsaucePlugin = {
     lib: /([a-zA-Z_\-/]+\/([0-9a-zA-Z_-]+)\/(baseline|outline|round|sharp)\.svg)/,
   },
   selector: (path: RegExpMatchArray) => `mi/${path[3]}/${path[2].replace(/[_]+/g, '-')}`,
-  path: `${resolve(join('node_modules'), '@material-icons/svg')}/svg/**/@(baseline|outline|round|sharp).svg`,
+  path: `${resolve('node_modules', '@material-icons/svg')}/svg/**/@(baseline|outline|round|sharp).svg`,
 }
 
 export default materialIconsPlugin
